b2e5b9f6e465fc8aaa03b56c0e2f836cfe34f7f8
Jutta G <jutta.ga@gmail.com>
2017-05-13 16:21:12 +0200

    change sumNum

--- R/sumNum.R
+++ R/sumNum.R
@@ -20,19 +20,29 @@ sumNum <- function(data, grp=NULL, rd=2){
   if(is.matrix(data)) data <- as.data.frame(data)
   num <- data %>% select_if(is.numeric)
 
+  if(length(grp)>2) stop("Maximum number of factors to group by is 2!")
+
   if(is.null(grp)){
-    sum <- num %>% summarise_each(funs(N=length(which(!is.na(.))), miss=length(which(is.na(.))), Minimum=min(., na.rm=T), Median=median(.,na.rm=T),
-                                       Maximum=max(.,na.rm=T), Mean=mean(.,na.rm=T), Sd=sd(.,na.rm=T)))
+    sum <- num %>% summarise_each(funs(N=length(which(!is.na(.))), miss=length(which(is.na(.))), Minimum=min(., na.rm=T), Q1=quantile(., na.rm=T)[2],
+                                       Median=median(.,na.rm=T), Q3=quantile(., na.rm=T)[4], Maximum=max(.,na.rm=T), Mean=mean(.,na.rm=T), Sd=sd(.,na.rm=T)))
     sum <- sum %>% gather(key, value) %>% separate(key, into = c("variable", "what"), sep = "\\_") %>% spread(what, value)
-    sum <- sum %>% dplyr::select(variable, N, miss, Minimum, Maximum, Median, Mean, Sd)
+    sum <- sum %>% dplyr::select(variable, N, miss, Minimum, Q1, Median, Q3, Maximum, Mean, Sd)
   }
-  else{
+  else if(length(grp)==1){
     num <- num %>% mutate(group=data[,grp])
     sum <- num %>% group_by(group) %>%
-      summarise_each(funs(N=length(which(!is.na(.))), miss=length(which(is.na(.))), Minimum=min(., na.rm=T), Median=median(.,na.rm=T),
-                          Maximum=max(.,na.rm=T), Mean=mean(.,na.rm=T), Sd=sd(.,na.rm=T)))
+      summarise_each(funs(N=length(which(!is.na(.))), miss=length(which(is.na(.))), Minimum=min(., na.rm=T), Q1=quantile(., na.rm=T)[2],
+                          Median=median(.,na.rm=T), Q3=quantile(., na.rm=T)[4], Maximum=max(.,na.rm=T), Mean=mean(.,na.rm=T), Sd=sd(.,na.rm=T)))
     sum <- sum %>% gather(key, value, -group) %>% separate(key, into = c("variable", "what"), sep = "\\_") %>% spread(what, value)
-    sum <- sum %>% dplyr::select(variable, group, N, miss, Minimum, Maximum, Median, Mean, Sd)
+    sum <- sum %>% dplyr::select(variable, group, N, miss, Minimum, Q1, Median, Q3, Maximum, Mean, Sd)
+  }
+  else{
+    num <- num %>% mutate(group1=data[,grp[1]], group2=data[,grp[2]])
+    sum <- num %>% group_by(group1, group2) %>%
+      summarise_each(funs(Minimum=min(., na.rm=T), Q1=quantile(., na.rm=T)[2], Median=median(.,na.rm=T), Q3=quantile(., na.rm=T)[4],
+                          Maximum=max(.,na.rm=T), Mean=mean(.,na.rm=T), Sd=sd(.,na.rm=T)))
+    sum <- sum %>% gather(key, value, -group1, -group2) %>% separate(key, into = c("variable", "what"), sep = "\\_") %>% spread(what, value)
+    sum <- sum %>% dplyr::select(variable, group1, group2, Minimum, Q1, Median, Q3, Maximum, Mean, Sd)
   }
   sum %>% mutate_if(is.numeric, funs(round(.,rd)))
 }
